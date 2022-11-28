09be42e3804bb7ffcadca3ac3a9ccb319f12df19
= <=>
2022-11-28 11:13:56 +0100

    feat: :sparkles: you can now switch to current week

--- src/lib/WeekSelector.svelte
+++ src/lib/WeekSelector.svelte
@@ -24,9 +24,33 @@
       selectedWeek.set(week - 1);
     }
   }
+  function setCurrentWeek() {
+    selectedWeek.set(
+      (() => {
+        const date = new Date();
+        date.setHours(0, 0, 0, 0);
+        date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+        const week1 = new Date(date.getFullYear(), 0, 4);
+        return (
+          1 +
+          Math.round(
+            ((date.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7
+          )
+        );
+      })()
+    );
+    selectedYear.set(
+      (() => {
+        const date = new Date();
+        date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+        return date.getFullYear();
+      })()
+    );
+  }
 </script>
 
 <div>
   <button on:click={decrementWeek}><p>vorige week</p></button>
+  <button on:click={setCurrentWeek}><p>deze week</p></button>
   <button on:click={incrementWeek}><p>volgende week</p></button>
 </div>
